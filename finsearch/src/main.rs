346e2f9e10bf937e0028b34b84884fb064423dd7
Elliot Kang <kkang2097@gmail.com>
2024-04-11 23:43:46 -0700

    fixing stuff, adding app state

--- finsearch/src/main.rs
+++ finsearch/src/main.rs
@@ -1,4 +1,5 @@
 use axum::{
+    extract::State,
     http::{StatusCode, Request, Response},
     response::{IntoResponse, Json},
     routing::post,
@@ -6,12 +7,23 @@ use axum::{
     Router,
     serve
 };
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use tower_http::{
     trace::TraceLayer
 };
+use std::env;
+use serde_json::{json, Value};
+use dotenv::dotenv;
+
+pub mod openai;
+pub mod brave;
+use crate::openai::IO_LLM;
+use crate::brave::BraveSearch;
+
+
 
 
 #[derive(Serialize, Deserialize)]
@@ -35,15 +47,15 @@ struct ChatCompletionResponse {
     choices: Vec<ChatMessage>,
 }
 
-async fn chat_completions(Json(request): Json<ChatCompletionRequest>) -> impl IntoResponse {
+async fn chat_completions(State(state): State<AppState>, Json(request): Json<ChatCompletionRequest>) -> impl IntoResponse {
     let resp_content = if let Some(first_message) = request.messages.first() {
         if first_message.role == "user" {
             format!("As a mock AI Assistant, I can only echo your last message: {}", request.messages.last().unwrap().content)
         } else {
-            "As a mock AI Assistant, I can only echo your last message, but there were no messages!".to_string()
+            "There were no messages!".to_string()
         }
     } else {
-        "As a mock AI Assistant, I can only echo your last message, but there were no messages!".to_string()
+        "There were no messages!".to_string()
     };
 
     let now = SystemTime::now()
@@ -63,11 +75,51 @@ async fn chat_completions(Json(request): Json<ChatCompletionRequest>) -> impl In
     })
 }
 
+
+#[derive(Clone)]
+struct AppState {
+    http_client: Client,
+    llm: openai::IO_LLM,
+    search_engine: brave::BraveSearch
+}
+
+
 #[tokio::main]
 async fn main() {
+
+    dotenv().ok();
+    
     tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
 
-    let app = Router::new().route("/chat/completions", post(chat_completions))
+
+    let llm_args: Value = json!({
+        "model": "gpt-3.5-turbo",
+        "temperature": 0.1
+    });
+    
+    let llm: IO_LLM = IO_LLM {
+        llm_args: llm_args,
+        api_key: env::var("OPENAI_API_KEY").unwrap_or_else(|_| "~/".to_string())
+    };
+
+    let app_state = AppState {
+        http_client: Client::new(),
+        llm: IO_LLM {
+            api_key: env::var("OPENAI_API_KEY").unwrap_or_else(|_| "~/".to_string()),
+            llm_args: json!({
+                "model": "gpt-3.5-turbo",
+                "temperature": 0.1
+            })
+        },
+        search_engine: brave::BraveSearch {
+            api_key: env::var("BRAVE_API_KEY").unwrap_or_else(|_| "~/".to_string()),
+        }
+    };
+
+
+    let app = Router::new()
+        .with_state(app_state)
+        .route("/chat/completions", post(chat_completions))
         .layer(TraceLayer::new_for_http());
     let listener = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
         serve(listener, app)
